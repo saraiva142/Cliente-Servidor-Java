c651fdb88f7a38c717b176fa6f9aaa5d4cbc9fef
saraiva142 <saraivajandrade@gmail.com>
2023-09-25 22:22:44 -0300

    Atualizacao entrada

--- src/Client.java
+++ src/Client.java
@@ -1,54 +1,70 @@
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws Exception {
         // 1. Estabelecer a conexão com o Server
         Socket conexao = new Socket("127.0.0.1", 8888);
+        System.out.println("Conexão estabelecida com o servidor.");
+
         // 2. Acessar os fluxos (streams) de entrada e saída de dados
         PrintWriter saida = new PrintWriter(conexao.getOutputStream(), true);
         BufferedReader entrada = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
 
+        Scanner scanner = new Scanner(System.in);
+
         while (true) {
+            // Exibir opções para o usuário
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Cadastrar");
+            System.out.println("2 - Login");
+            System.out.println("3 - Sair");
+            System.out.print("Opção: ");
+            
+            int opcao = scanner.nextInt();
+            scanner.nextLine(); // Limpar a nova linha pendente
 
-            // 3.Client vai enviar mensagem para o Server
-            System.out.println("Para Cadastrar Escreva: 'CADASTRAR usuario senha nome/sobrenome email telefone'");
-            System.out.println("Para fazer Login Escreva: 'LOGIN usuario senha'");
-            System.out.println("Cliente >> ");
-            String msg = new Scanner(System.in).nextLine();
-            saida.println(msg);
+            if (opcao == 1) {
+                // Cadastro de um novo usuário
+                System.out.print("Digite o nome de usuário: ");
+                String username = scanner.nextLine();
+                System.out.print("Digite a senha: ");
+                String password = scanner.nextLine();
+                System.out.print("Digite o nome completo: ");
+                String nomeCompleto = scanner.nextLine();
+                System.out.print("Digite o email: ");
+                String email = scanner.nextLine();
+                System.out.print("Digite o telefone: ");
+                String telefone = scanner.nextLine();
 
-            // 4. Receber os dados do Server
-            String msg2 = entrada.readLine();
-            System.out.println(msg2);
+                // Enviar solicitação de cadastro ao servidor
+                String mensagem = "CADASTRAR " + username + " " + password + " " + nomeCompleto + " " + email + " " + telefone;
+                saida.println(mensagem);
+            } else if (opcao == 2) {
+                // Login de usuário
+                System.out.print("Digite o nome de usuário: ");
+                String username = scanner.nextLine();
+                System.out.print("Digite a senha: ");
+                String password = scanner.nextLine();
 
-            // 5. Exemplo de autenticação
-            if (msg.startsWith("LOGIN")) {
-                String[] parts = msg.split(" ");
-                if (parts.length == 3) {
-                    String username = parts[1];
-                    String password = parts[2];
-                    System.out.println("Tentando autenticar...");
-                } else {
-                    System.out.println("Formato incorreto. Use: LOGIN nome senha");
-                }
+                // Enviar solicitação de login ao servidor
+                String mensagem = "LOGIN " + username + " " + password;
+                saida.println(mensagem);
+            } else if (opcao == 3) {
+                // Encerrar a conexão e sair
+                conexao.close();
+                System.out.println("Conexão encerrada. Saindo...");
+                break;
+            } else {
+                System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
             }
 
-            // 6. Exemplo de cadastro de um novo usuário
-            if (msg.startsWith("CADASTRAR")) {
-                String[] parts = msg.split(" ");
-                if (parts.length == 6) {
-                    String username = parts[1];
-                    String password = parts[2];
-                    String nome_completo = parts[3];
-                    String email = parts[4];
-                    String telefone = parts[5];
-                    System.out.println("Cadastro de usuário em andamento...");
-                } else {
-                    System.out.println("Formato incorreto. Use: CADASTRAR nome senha nome_completo email telefone");
-                }
-            }
+            // Receber a resposta do servidor e exibi-la
+            String resposta = entrada.readLine();
+            System.out.println("Servidor >> " + resposta);
         }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,108 +1,76 @@
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.io.*;
-import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) throws Exception {
         // 1. Escutar a porta de serviço 8888
         ServerSocket server = new ServerSocket(8888);
-        // 2. Aguardar conexão do Cliente
-        Socket conexao = server.accept();
-        // 3. Obter os fluxos de entrada e saida
-        PrintWriter saida = new PrintWriter(conexao.getOutputStream(), true);
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
-
-        // 4. Configurar a conexão com o banco de dados SQLite
-        String url = "jdbc:sqlite:C:\\Users\\Elisio da Silva\\docker\\base.db"; // Caminho absoluto para o banco de dados
-        Connection connection = DriverManager.getConnection(url);
-
-        //Teste de Conexão
-        try {
-            connection = DriverManager.getConnection(url);
-            System.out.println("Conexão com o banco de dados estabelecida com sucesso.");
-        } catch (SQLException e) {
-            System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
-            e.printStackTrace();
-            return; // Saia do programa em caso de erro na conexão
-        }
-
+        System.out.println("Servidor aguardando conexões...");
         
-
         while (true) {
-            // 4. Receber os dados do cliente
-            String msg = entrada.readLine();
-            System.out.println(msg);
+            // 2. Aguardar conexão do Cliente
+            Socket conexao = server.accept();
+            System.out.println("Cliente conectado: " + conexao.getInetAddress().getHostAddress());
+            
+            // 3. Obter os fluxos de entrada e saída
+            PrintWriter saida = new PrintWriter(conexao.getOutputStream(), true);
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
 
-            // 5. Exemplo de inserção de um novo usuário na tabela
-            if (msg.startsWith("INSERIR")) {
-                String[] parts = msg.split(" ");
-                if (parts.length == 3) {
-                    String username = parts[1];
-                    String password = parts[2];
-                    insertUser(connection, username, password);
-                    saida.println("Novo usuário inserido com sucesso.");
-                } else {
-                    saida.println("Formato incorreto. Use: INSERIR nome senha");
-                } 
-            }
-            // 6. Autenticação do usuário
-            if (msg.startsWith("LOGIN")) {
-                String[] parts = msg.split(" ");
-                if (parts.length == 3) {
-                    String username = parts[1];
-                    String password = parts[2];
-                    if (authenticateUser(connection, username, password)) {
-                        saida.println("Login Successful");
-                    } else {
-                        saida.println("Falha na autenticação. Verifique nome de usuário e senha.");
-                    }
-                } else {
-                    saida.println("Login Failed. Use: LOGIN nome senha");
-                }
-            } else if (msg.startsWith("CADASTRAR")) { //Cadastrar informações do usuário
-                String[] parts = msg.split(" ");
+            // 4. Configurar a conexão com o banco de dados SQLite
+            String url = "jdbc:sqlite:C:\\Users\\Elisio da Silva\\docker\\base.db"; // Caminho absoluto para o banco de dados
+            Connection connection = DriverManager.getConnection(url);
+
+            while (true) {
+                // 5. Receber os dados do cliente
+                String msg = entrada.readLine();
+                System.out.println("Cliente >> " + msg);
+
+                // 6. Exemplo de inserção de um novo usuário na tabela
+                if (msg.startsWith("CADASTRAR")) {
+                    String[] parts = msg.split(" ");
                     if (parts.length == 6) {
                         String username = parts[1];
                         String password = parts[2];
                         String nome_completo = parts[3];
                         String email = parts[4];
                         String telefone = parts[5];
-                        registerUser(connection, username, password, nome_completo, email, telefone);
+                        insertUser(connection, username, password, nome_completo, email, telefone);
                         saida.println("Cadastro de usuário realizado com sucesso.");
                     } else {
                         saida.println("Formato incorreto. Use: CADASTRAR nome senha nome_completo email telefone");
                     }
-            } else {
-                // 6. Enviar resposta para o cliente
-                System.out.print("Servidor >> ");
-                String msg2 = new BufferedReader(new InputStreamReader(System.in)).readLine();
-                saida.println(msg2);
+                }
+                // 7. Autenticação do usuário
+                else if (msg.startsWith("LOGIN")) {
+                    String[] parts = msg.split(" ");
+                    if (parts.length == 3) {
+                        String username = parts[1];
+                        String password = parts[2];
+                        if (authenticateUser(connection, username, password)) {
+                            saida.println("Login Successful");
+                        } else {
+                            saida.println("Falha na autenticação. Verifique nome de usuário e senha.");
+                        }
+                    } else {
+                        saida.println("Formato incorreto. Use: LOGIN nome senha");
+                    }
+                } else {
+                    saida.println("Comando não reconhecido.");
+                }
             }
-
         }
     }
 
     // Método para inserir um novo usuário na tabela
-    private static void insertUser(Connection connection, String username, String password) {
-        String sql = "INSERT INTO user (name, passworld) VALUES (?, ?)";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1, username);
-            statement.setString(2, password);
-            statement.executeUpdate();
-            System.out.println("Novo usuário inserido com sucesso.");
-        } catch (SQLException e) {
-            System.err.println("Erro ao inserir novo usuário: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    // Método para cadastrar um novo usuário na tabela
-    private static void registerUser(Connection connection, String username, String password, String nome_completo, String email, String telefone) {
+    private static void insertUser(Connection connection, String username, String password, String nome_completo, String email, String telefone) {
         String sql = "INSERT INTO user (name, passworld, nome_completo, email, telefone) VALUES (?, ?, ?, ?, ?)";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, username);
@@ -132,8 +100,4 @@ public class Server {
             return false;
         }
     }
-
 }
-
-
-
